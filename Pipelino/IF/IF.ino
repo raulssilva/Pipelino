b05abffd680641e5b0807cff7f430c63465b9305
Raul Silveira <raul-brasil@hotmail.com>
2015-11-29 13:47:17 -0300

    Modificações no IF

--- Pipelino/IF/IF.ino
+++ Pipelino/IF/IF.ino
@@ -10,8 +10,8 @@
  ** CS - pin 4
  
  The circuit (LCD 16x2):
- * LCD RS pin to digital pin 10
- * LCD Enable pin to digital pin 9
+ * LCD RS pin to digital pin 9
+ * LCD Enable pin to digital pin 8
  * LCD D4 pin to digital pin 5
  * LCD D5 pin to digital pin 6
  * LCD D6 pin to digital pin 3
@@ -29,7 +29,13 @@
 #include <LiquidCrystal.h>
 
 #define CHIP_SELECT 4
-LiquidCrystal lcd(10, 9, 5, 6, 3, 2);
+LiquidCrystal lcd(9, 8, 5, 6, 3, 2);
+
+const String operators[] = {"ADD","SUB","ADDI","LW","SW","SLL","SRL","BEQ","BNE"};
+
+const String registers[] = {"$0","$s0","$s1","$s2","$s3","$s4","$s5","$s6","$s7"};
+
+#define CLOCK 10000
 
 void setup(){
   lcd.begin(16, 2);
@@ -59,50 +65,101 @@ void setup(){
     String param1 = "";
     String param2 = "";
     String param3 = "";
-    String instruction = "";
     
     // Read byte-by-byte from the file until there's nothing else in it
     while (assemblyFile.available()) {
       if(opCode == ""){
         opCode = readParam(assemblyFile);
+        for(byte i = 0; i<9; i++){
+          if(operators[i] == opCode){
+            Serial.print(i);
+          }
+        }
       }
       
       switch(opCode.length()){
         case 2:
           //Load ou Store
           param1 = readParam(assemblyFile);
-          param2 = readParam(assemblyFile);
+          param2 = readParamConst(assemblyFile);
+          param3 = readParam(assemblyFile);
+          param3 = param3.substring(0,param3.length()-1);
+          
+          for(byte i = 0; i<9; i++){
+            if(registers[i] == param1){
+              Serial.print(i);
+              Serial.print(param2.toInt());
+            }
+          }
+          
+          for(byte i = 0; i<9; i++){
+            if(registers[i] == param3){
+              Serial.print(i);
+            }
+          }
+          param3 = "(" + param3 + ")";
           break;
         case 3:
           //ADD, SUB, BEQ, BNE, SLL e SRL
           param1 = readParam(assemblyFile);
           param2 = readParam(assemblyFile);
           param3 = readParam(assemblyFile);
+          
+          for(byte i = 0; i<9; i++){
+            if(registers[i] == param1){
+              Serial.print(i);
+            }
+          }
+          
+          for(byte i = 0; i<9; i++){
+            if(registers[i] == param2){
+              Serial.print(i);
+            }
+          }
+          
+          if(opCode == "ADD" || opCode == "SUB"){
+            for(byte i = 0; i<9; i++){
+              if(registers[i] == param3){
+                Serial.print(i);
+              }
+            }
+          }else{
+            Serial.print(param3.toInt());
+          }
           break;
         default:
           //ADDI
           param1 = readParam(assemblyFile);
           param2 = readParam(assemblyFile);
           param3 = readParam(assemblyFile);
+          
+          for(byte i = 0; i<9; i++){
+            if(registers[i] == param1){
+              Serial.print(i);
+            }
+          }
+          
+          for(byte i = 0; i<9; i++){
+            if(registers[i] == param2){
+              Serial.print(i);
+              Serial.print(param3.toInt());
+            }
+          }
       }
-      
-      instruction = opCode + " " + param1 + " " + param2 + " " + param3;
       
       lcd.clear();
       lcd.setCursor(6,0);
       lcd.print(opCode);
       lcd.setCursor(2,1);
       lcd.print(param1 + " " + param2 + " " + param3);
-      
-      Serial.print(instruction);
-      delay(3000);
+
+      delay(CLOCK);
       
       opCode = "";
       param1 = "";
       param2 = "";
       param3 = "";
 
-//      Serial.write(assemblyFile.read());
     }
     assemblyFile.close();
     
@@ -129,6 +186,19 @@ String readParam(File assemblyFile){
   while (assemblyFile.available()) {
     aux = assemblyFile.read();
     if(aux == ' ' || aux == '\n' || aux == '\t'){
+      return param;
+    }else{
+      param += aux;
+    }
+  }
+}
+
+String readParamConst(File assemblyFile){
+  char aux;
+  String param = "";
+  while (assemblyFile.available()) {
+    aux = assemblyFile.read();
+    if(aux == '('){
       return param;
     }else{
       param += aux;
